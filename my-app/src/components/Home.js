e66603766bddbd5baf57ebee292f3e55e2b5f2bc
JohaGigaSoft <joha.gigasoft@gmail.com>
2022-06-04 16:20:27 +0600

    added the react app here

---
+++ my-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Home=()=>{
+   return(
+       <>
+        <h1>Home Page Now</h1>
+        <p>this is a paragraph tag</p>
+        <p>React component</p>
+        
+       </>
+   
+   )
+    
+}
+export default Home;
